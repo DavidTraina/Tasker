679c336ba86c34b8705ac769165a7dd68562f041
David Traina <david.traina905@gmail.com>
2020-09-02 12:47:34 -0400

    commenting

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 
 db = OrderedDict()
 
+
 class Task():
     def __init__(self, content, date_created):
         self.content = content
@@ -18,7 +19,7 @@ class Task():
 @app.route('/', methods=['POST', 'GET'])
 def add():
     if request.method == 'POST':
-        task_content = request.form['content']
+        task_content = request.form['content']  # 'content' refers to name of text input in form in index.html
         task_id = uuid.uuid4()
         new_task = Task(task_content,
                         datetime.datetime.now(tz=datetime.timezone.utc))
